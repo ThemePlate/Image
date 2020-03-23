7c00083b7de21424237b424fbcd5f400df8bfab4
kermage <genealyson.torcende@gmail.com>
2020-03-24 07:23:02 +0800

    the working concept

--- Image.php
+++ Image.php
@@ -10,4 +10,40 @@
 namespace ThemePlate;
 
 class Image {
+
+	private static $sizes = array();
+	private static $manipulations = array();
+
+
+	public static function register( $name, $width, $height ) {
+
+		self::$sizes[ $name ] = compact( 'width', 'height' );
+
+		return self::$sizes;
+
+	}
+
+
+	public static function manipulate( $size, $filter, $args ) {
+
+		self::$manipulations[ $size ][] = compact( 'filter', 'args' );
+
+		return self::$manipulations;
+
+	}
+
+
+	public static function get_html( $attachment_id, $size ) {
+
+		return wp_get_attachment_image( $attachment_id, $size );
+
+	}
+
+
+	public static function get_url( $attachment_id, $size ) {
+
+		return wp_get_attachment_image_url( $attachment_id, $size );
+
+	}
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ThemePlate Image
+
+## Usage
+
+```php
+use ThemePlate\Image;
+
+Image::register( 'wanted_size', 1920, 1080 );
+Image::manipulate( 'wanted_size', 'pixilate', 10 );
+Image::get_html( 'attachment_id', 'wanted_size' );
+Image::get_url( 'attachment_id', 'wanted_size' );
+```
