e0a6dc4a9528dcd91d89fc7207603ab218ade566
kermage <genealyson.torcende@gmail.com>
2020-03-24 07:48:59 +0800

    test with the 'intervention/image' library

--- Image.php
+++ Image.php
@@ -9,10 +9,13 @@
 
 namespace ThemePlate;
 
+use Intervention\Image\ImageManager;
+
 class Image {
 
 	private static $sizes = array();
 	private static $manipulations = array();
+	private static $manager;
 
 
 	public static function register( $name, $width, $height ) {
@@ -43,6 +46,29 @@ class Image {
 	public static function get_url( $attachment_id, $size ) {
 
 		return wp_get_attachment_image_url( $attachment_id, $size );
+
+	}
+
+
+	private static function filter( $file, $name, $args ) {
+
+		if ( ! self::$manager ) {
+			self::$manager = new ImageManager( array( 'driver' => 'gd' ) );
+		}
+
+		$image = self::$manager->make( $file );
+
+		return call_user_func_array( array( $image, $name ), (array) $args );
+
+	}
+
+
+	public static function test( $attachment_id ) {
+
+		$file  = get_attached_file( $attachment_id );
+		$image = self::filter( $file, 'pixelate', 10 );
+
+		$image->save( $file );
 
 	}
 
